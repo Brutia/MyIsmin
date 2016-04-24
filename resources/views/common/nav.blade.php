40eda8879d0c2fd202c6f5eda72d5dd59d3041fc
brutia <guillaume.andres@yahoo.fr>
2016-04-24 14:29:22 +0200

    Translation de certaines vues + correction migrations

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -76,8 +76,18 @@
 <!--                     <a href="#">Innov'Action</a> -->
 <!--                 </li> -->
 				@if( Auth::user())
-				<li>
-					<a href={{URL::to('/logout') }}>Se déconnecter</a>
+					
+				<li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mon Compte<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{URL::to('/logout')}}">Se déconnecter</a></li>
+                        @role('admin')
+                        	<li><a href="{{URL::to('/admin/user')}}">Gestion des utilisateurs</a></li>
+                        @endrole
+                        @role('admin','prez')
+                        	<li><a href="{{URL::to('/admin/event')}}">Calendrier</a></li>
+                        @endrole
+                    </ul>
 				</li>
 				@else
                 <li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
-                    
+                    	@if (!Auth::guest())
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
@@ -61,6 +61,7 @@
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
+                        @endif
                 </ul>
             </div>
         </div>
